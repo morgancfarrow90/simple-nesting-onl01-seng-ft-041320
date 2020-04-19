ee9f8b48c0c968d8f5b1f11b6d8b51fe28ecd3dc
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-19 20:09:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,8 +63,6 @@ def dennis_ritchies_language
 programmer_hash[:dennis_ritchie][:languages][0]
 end
 
-dennis_ritchie
-
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
