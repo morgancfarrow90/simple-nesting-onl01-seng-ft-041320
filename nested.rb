f59489c57b749ea68d86f0a6bfdd434336f932aa
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-19 19:09:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -42,22 +42,7 @@ def alan_kay_is_known_for
 	
 alan_kay_is_known_for
 	
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-end
+	   
 
 def dennis_ritchies_language
 	programmer_hash = 
